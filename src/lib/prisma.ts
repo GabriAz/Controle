bbf1218974287cd48957a8c43b622d80fb6d39ba
gabriel <gabrielazevedodesouzaguimaraes@gabriels-MacBook-Air.local>
2026-02-26 16:48:17 -0300

    chore: prepare for Render and MySQL (Hostinger)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,24 +1,11 @@
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-
-const connectionString = `${process.env.DATABASE_URL}`;
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-let prisma: PrismaClient;
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    });
 
-if (process.env.NODE_ENV === "production") {
-    const pool = new Pool({ connectionString });
-    const adapter = new PrismaPg(pool);
-    prisma = new PrismaClient({ adapter });
-} else {
-    if (!globalForPrisma.prisma) {
-        const pool = new Pool({ connectionString });
-        const adapter = new PrismaPg(pool);
-        globalForPrisma.prisma = new PrismaClient({ adapter });
-    }
-    prisma = globalForPrisma.prisma;
-}
-
-export { prisma };
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
